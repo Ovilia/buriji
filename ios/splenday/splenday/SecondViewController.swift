954645cecc34244e14d05981e2fe417528f348a4
Ovilia <zwl.sjtu@gmail.com>
2015-10-01 15:47:21 +0800

    init with ios

---
+++ ios/splenday/splenday/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  splenday
+//
+//  Created by ZhangOvilia on 10/1/15.
+//  Copyright © 2015 Ovilia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
